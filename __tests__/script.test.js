09e7dad38659389e39064f047237a6d33e5d9408
Dominic Simpson <db_simpson@yahoo.co.uk>
2025-07-09 12:18:43 +0100

    Added tests

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -1,3 +1,34 @@
-test('basic test', () => {
-  expect(1 + 1).toBe(2);
-});
+const { renderDOM } = require('./helpers');
+global.fetch = require('node-fetch');
+
+let dom;
+let document;
+
+describe('index.html', () => {
+  beforeEach(async () => {
+    dom = await renderDOM('./index.html');
+    document = await dom.window.document;
+  })
+  
+  it('has a reset zoom button with an icon', () => {
+    const btn = document.querySelector('#reset-zoom')
+    expect(btn).toBeTruthy();
+
+    const icon = btn.querySelector('i');
+    expect(icon).toBeTruthy();
+    expect(icon.classList.contains('fa-compress')).toBe(true);
+  });
+
+  it('can trigger a click event on the reset button', () => {
+    const btn = document.querySelector('#reset-zoom');
+    let clicked = false;
+
+    btn.addEventListener('click', () => {
+      clicked = true;
+    });
+
+    btn.click();
+    expect(clicked).toBe(true);
+  })
+})
+
